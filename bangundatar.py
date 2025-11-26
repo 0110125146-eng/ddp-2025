7f2d637954cc2f7c2c6665210501eaa649c08f8a
0110125146-eng <0110125146@student.nurulfikri.ac.id>
2025-11-26 11:41:08 +0700

    Add files via upload

---
+++ bangundatar.py
@@ -0,0 +1,19 @@
+def jajarGanjar(alas, tinggi):
+    return alas * tinggi 
+print(jajarGanjar(4, 7))
+
+def luas_persegi(sisi):
+    return sisi * sisi
+print(luas_persegi(5))
+
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
+print(luas_persegi_panjang(20, 10))
+
+def luas_segitiga(alas, tinggi):
+    return alas * tinggi
+print(luas_segitiga(10, 5))
+
+def luas_lingkaran(jari_jari):
+    return jari_jari **2
+print(luas_lingkaran(8))
